52d6def880a44d5a4c9b8211b7b16be43354814e
Skye Im <skye.im@nyu.edu>
2019-04-16 03:11:00 +0000

    Feat bot: Fix reporting channel

--- bot.py
+++ bot.py
@@ -25,19 +25,20 @@ else:
 
 timefmt = '%Y-%m-%d %H:%M:%S'
 async def get_logs():
-    channel = client.get_channel(CHANNEL_ID)
+    snipchannel = client.get_channel(CHANNEL_ID)
+    reportchannel = client.get_channel(SNIP_DIFF_REPORT_CHANNEL)
     data = []
     counter = 0
 
     # get last msgid from discord
     lastmsgid = DiscordMessage.objects.order_by('-timestamp').first().messageid
-    lastmsg = await client.get_message(channel, lastmsgid)
+    lastmsg = await client.get_message(snipchannel, lastmsgid)
 
 
     kwargs = {'after': lastmsg}
 
     # Gets up to 100 messages since last importing.
-    async for msg in client.logs_from(channel, limit=100, **kwargs, reverse=True):
+    async for msg in client.logs_from(snipchannel, limit=100, **kwargs, reverse=True):
         data.append({
             'timestamp': msg.timestamp.strftime('%Y-%m-%d %H:%M:%S'),
             'authorid': msg.author.id,
@@ -93,7 +94,6 @@ async def get_logs():
                 process_snip(snip)
 
         # Print success
-        channel = client.get_channel(SNIP_DIFF_REPORT_CHANNEL)
         formatstring = '{:<32}  {:>2}  {:>3}'
         lines = ['**Snip Archive Update** ```', formatstring.format('Username', '+', '=')]
         for authorid in newsnips:
@@ -105,30 +105,24 @@ async def get_logs():
         content = '\n'.join(lines)
         if IS_DRY_RUN:
             content = '===THIS IS A DRY RUN===\n' + content
-        await client.send_message(channel, content)
+        await client.send_message(reportchannel, content)
     else:  # No snips to update
         content = 'No new snips since last run.'
         if IS_DRY_RUN:
             content = '===THIS IS A DRY RUN===\n' + content
-        await client.send_message(channel, content)
-
-    print('Retrieved {} messages, stored.'.format(counter))
-    sys.exit(0)
+        await client.send_message(reportchannel, content)
 
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    # scrape the missed logs on startup
-    await get_logs()
-
-
-@client.event
-async def on_message(message):
-    if message.author.id == '164963698274729985':
-        pass
-    print(message.author, message.content)
+    print('Logged in as' + client.user.name)
+    try:
+        # scrape the missed logs on startup
+        await get_logs()
+    except Exception:
+        import traceback
+        traceback.print_exc()
+    sys.exit(0)
 
 
 client.run(os.environ['BOTTOKEN'])
